437f5b75a94a11ba159ad6a88cfa0ace6ea25803
Thiago do Vale Cabral <thiago.cabral@ice.ufjf.br>
2021-08-14 17:00:11 -0300

    feat: App throw

--- src/main/java/br/ufjf/App.java
+++ src/main/java/br/ufjf/App.java
@@ -9,6 +9,9 @@ import java.io.FileReader;
 public class App {
 
     public static void main( String[] args ) throws Exception {
+        if(args.length < 2) {
+            throw new Exception("xml and txt file paths are needed");
+        }
         System.out.println(args[0]);
         DFA dfa = XmlReader.xmlToDFA(args[0]);
         LexicalAnalyser la = new LexicalAnalyser(dfa, new FileReader(args[1]));
